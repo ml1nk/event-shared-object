fd2d4d63e1c7c3bcb7d30220fbe3e2da3278fb6d
Marcel Link <marcel.link@t-online.de>
2018-12-30 15:10:08 +0100

    + on/off on shared object

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+const eventemitter3 = require("eventemitter3");
+
 class Shared {
 
-    constructor(name, evcom, evout, data) {
-        this.name = name;
-        this.evcom = evcom;
-        this.evout = evout;
-        this.master = typeof data === "object";
-        this.data = master ? data : null;
-        this.rev = 0;
-        master ? _master() : _slave();
+    constructor(name, evcom, obj) {
+        this._name = name;
+        this._evcom = evcom;
+        this._evout = new eventemitter3;
+        this._obj = this._ismaster ? obj : null;
+        this._rev = 0;
+        this._reg = [{}];
+        typeof obj === "object" ? this._master() : this._slave();
+    }
+
+    on(pos, cb) {
+        pos.reduce((o, i) => {
+            if(!o[0].hasOwnProperty(i)) {
+                o[0][i] = [{}];
+            }
+            return o[0][i]; 
+        }, this._reg).push(cb);
+    }
+
+    off(pos, cb) {
+        let path = [];
+        let cur = this._reg;
+
+        for(let i=0; i<pos.length; i++) {
+            if(!cur[0].hasOwnProperty(pos[i])) return false;
+            path.push(cur);
+            cur = cur[0][pos[i]];
+        }
+
+        for(let i=1; i<cur.length;i++) {
+            if(cur[i] !== cb) continue;
+            if(cur.length>2 || Object.keys(cur[0]).length>0 || path.length===0) {
+                cur.splice(i, 1);
+            } else {
+                let p = path.length-2;
+                for(; p>=0;p--) {
+                    if(path[p].length>2 || Object.keys(path[p][0].length>0)) break;
+                }
+                delete path[p][0][pos[p]];
+            }
+            return true;
+        }
+        return false;
     }
 
     _slave() {
@@ -16,22 +53,27 @@ class Shared {
 
     _master() {
         evcom.on(name, (rev, set, pos, data, cb) => {
-            if (obj.rev !== rev) {
+            if (rev !== this.rev) {
                 cb(false);
                 return;
             }
-
-
+            _emit(obj, set, pos, data)
+            _apply(obj, set, pos, data);
+            cb(true);
         });
     }
+}
 
+function _emit(obj, set, pos, data) {
+    let pre = pos.slice(0, -1).reduce((o, i) => o[i], obj);
 
 }
 
 function _apply(obj, set, pos, data) {
     let pre = pos.slice(0, -1).reduce((o, i) => o[i], obj);
-    set ? pre[pos.slice(-1)[0]] = data : delete pre[pos.slice(-1)[0]];
+    set ? pre[pos[pos.length-1]] = data : delete pre[pos[pos.length-1]];
 }
 
 
 exports._apply = _apply;
+exports.Shared = Shared;
--- test/test-shared.js
+++ test/test-shared.js
@@ -1,54 +1,153 @@
 const expect = require('chai').expect;
 const path = require('path');
-const api = require(path.resolve(__dirname,"..","index.js"));
+const api = require(path.resolve(__dirname, "..", "index.js"));
 
 
 /**
  * Applying a change (set/del) to an object
  */
-it('apply', ()=>{
-    let obj = { 
-        "a" : "b",
-        "c" : { 
-            "d" : "e",
-            "e" : "f"
+it('apply', () => {
+    let obj = {
+        "a": "b",
+        "c": {
+            "d": "e",
+            "e": "f"
         }
     }
-    
-    api._apply(obj, true, ["a"],"g");
-    
+
+    api._apply(obj, true, ["a"], "g");
+
     expect(obj).to.deep.equal({
-        "a" : "g",
-        "c" : { 
-            "d" : "e",
-            "e" : "f"
+        "a": "g",
+        "c": {
+            "d": "e",
+            "e": "f"
         }
     });
 
     api._apply(obj, false, ["a"]);
-    
+
     expect(obj).to.deep.equal({
-        "c" : { 
-            "d" : "e",
-            "e" : "f"
+        "c": {
+            "d": "e",
+            "e": "f"
         }
     });
 
-    api._apply(obj, false, ["c","d"]);
-    
+    api._apply(obj, false, ["c", "d"]);
+
     expect(obj).to.deep.equal({
-        "c" : { 
-            "e" : "f"
+        "c": {
+            "e": "f"
         }
     });
 
-    api._apply(obj, true, ["c", "e"], { "z" : "h"});
-    
+    api._apply(obj, true, ["c", "e"], {
+        "z": "h"
+    });
+
     expect(obj).to.deep.equal({
-        "c" : { 
-            "e" : { 
-                "z" : "h"
+        "c": {
+            "e": {
+                "z": "h"
             }
         }
     });
+});
+
+it('_reg', () => {
+    let sh = new api.Shared();
+    let a = () => {};
+    let b = () => {};
+    let c = () => {};
+
+    expect(sh._reg).to.deep.equal([{}]);
+
+    sh.on(["a", "b"], a);
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    }]);
+
+    expect(sh.off(["a", "b"], a)).to.be.true;
+
+    expect(sh._reg).to.deep.equal([{}]);
+
+    sh.on([], b);
+
+    expect(sh._reg).to.deep.equal([{},b]);
+
+    sh.on([], c);
+
+    expect(sh._reg).to.deep.equal([{},b,c]);
+
+    expect(sh.off([], b)).to.be.true;
+
+    expect(sh._reg).to.deep.equal([{},c]);
+
+    expect(sh.off([], c)).to.be.true;
+
+    expect(sh._reg).to.deep.equal([{}]);
+
+    sh.on(["a", "b"], a);
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    }]);
+
+    sh.on([], b);
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    },b]);
+
+    sh.on([], c);
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    },b,c]);
+
+    expect(sh.off([], b)).to.be.true;
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    },c]);
+
+    expect(sh.off([], c)).to.be.true;
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a]
+        }]
+    }]);
+
+    sh.on(["a", "b"], c);
+
+    expect(sh._reg).to.deep.equal([{
+        a: [{
+            b: [{}, a, c]
+        }]
+    }]);
+
+    expect(sh.off([], c)).to.be.false;
+    expect(sh.off(["a"], c)).to.be.false;
+    expect(sh.off(["a","b","c"], c)).to.be.false;
+    expect(sh.off(["a","c"], c)).to.be.false;
+    expect(sh.off(["a","b"], b)).to.be.false;
+    expect(sh.off(["a","b"], a)).to.be.true;
+    expect(sh.off(["a","b"], a)).to.be.false;
+    expect(sh.off(["a","b"], c)).to.be.true;
+    expect(sh.off(["a","b"], c)).to.be.false;
+
+    expect(sh._reg).to.deep.equal([{}]);
 });
